10b239d80c2fbd96371181e71bdc2d09445f7d38
mrozonpw <wojciech.barszcz.stud@pw.edu.pl>
2026-01-03 23:26:59 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,19 +1,20 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
-#pragma once
-#include <vector>
+#include <cmath>
 #include <cstdint>
+#include <vector>
 
-using calc_function_t = double (*)(const std::vector<double>&, uint32_t);
-
-// funkcje testowe
-double calc_quadratic_function(const std::vector<double>& x, uint32_t n);
-double calc_woods_function(const std::vector<double>& x, uint32_t n);
-double calc_powell_singular_function(const std::vector<double>& x, uint32_t n);
-
-// punkty startowe
+double calc_quadratic_function(const std::vector<double>& x, const uint32_t n);
 std::vector<double> make_quadratic_x0(uint32_t n);
+double l2_norm(const std::vector<double>& x);
+
+
+double calc_woods_function( const std::vector<double>& x, const uint32_t n);
 std::vector<double> make_woods_x0(uint32_t n);
+double l2_norm_distance_to_woods_min(const std::vector<double>& x, uint32_t n);
+
+double calc_powell_singular_function(const std::vector<double>& x, uint32_t n);
 std::vector<double> make_powell_x0(uint32_t n);
+double l2_norm_distance_to_powell_min(const std::vector<double>& x, uint32_t n);
 
 #endif //FUNCTIONS_H
